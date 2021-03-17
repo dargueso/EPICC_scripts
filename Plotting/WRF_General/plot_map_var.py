0db38ab09db326faa34840b699ff7dfbb67bf5da
Daniel Argueso <d.argueso@uib.es>
2021-03-17 10:54:11 +0100

    [Beta] Create plot map var

---
+++ Plotting/WRF_General/plot_map_var.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+#####################################################################
+# Author: Daniel Argueso <daniel>
+# Date:   2021-03-17T09:40:51+01:00
+# Email:  d.argueso@uib.es
+# Last modified by:   daniel
+# Last modified time: 2021-03-17T09:40:54+01:00
+#
+# @Project@
+# Version: x.0 (Beta)
+# Description:
+#
+# Dependencies:
+#
+# Files:
+#
+#####################################################################
+"""
+
+
+import xarray as xr
+import numpy as np
+import netCDF4 as nc
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+import wrf
+import string
+
+from ..lib import visualization as vis
+from ..lib import io
+from .. import config as cfg
+
+
+varname = ['PRNC']
+
+io.load_wrf()
