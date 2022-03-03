b68c1e93bde09c25bcbc12fb7c78ffefdf3d8a17
Daniel Argüeso <d.argueso@unsw.edu.au>
2022-03-03 12:33:29 +0100

    Update Calc_land_sum_files_IDF.py

--- WRF_processing/Calc_land_sum_files_IDF.py
+++ WRF_processing/Calc_land_sum_files_IDF.py
@@ -16,7 +16,7 @@ lm_is=lm_is.rename({'west_east':'x','south_north':'y'})
 lm_is.coords['y']=lats[:,0]
 lm_is.coords['x']=lons[0,:]
 
-# reg = 'BAL'
+reg =''
 
 #####################################################################
 #####################################################################
@@ -24,23 +24,25 @@ lm_is.coords['x']=lons[0,:]
 wrun_pre = cfg.wrf_runs[0]
 wrun_fut = wrun_pre.replace("ERA5","ERA5_CMIP6anom")
 
-#fin_pre = xr.open_dataset(f'{cfg.path_in}/{wrun_pre}/hist2d_IFD_spell_hist2d_2013-2020.nc')
-fin_fut = xr.open_dataset(f'{cfg.path_in}/{wrun_fut}/hist2d_IFD_spell_hist2d_2013-2020.nc')
+fin_pre = xr.open_dataset(f'{cfg.path_in}/{wrun_pre}/hist2d_resample_time_IFD_2013-2020.nc')
+fin_fut = xr.open_dataset(f'{cfg.path_in}/{wrun_fut}/hist2d_resample_time_IFD_2013-2020.nc')
+#fin_fut = xr.open_dataset(f'{cfg.path_in}/{wrun_fut}/hist2d_IFD_spell_2013-2020.nc')
 #fin_fut = xr.open_dataset(f'{cfg.path_in}/{wrun_fut}/hist2d_IFD_resample_time_2013-2020.nc')
 
 
 #####################################################################
 #####################################################################
 
-#Region
-#fin_pre_land = fin_pre.where(lm_is==1).sel(y=slice(cfg.reg_coords[reg][0],cfg.reg_coords[reg][2]),x=slice(cfg.reg_coords[reg][1],cfg.reg_coords[reg][3])).sum(dim=('x','y'))
-#fin_fut_land = fin_fut.where(lm_is==1).sel(y=slice(cfg.reg_coords[reg][0],cfg.reg_coords[reg][2]),x=slice(cfg.reg_coords[reg][1],cfg.reg_coords[reg][3])).sum(dim=('x','y'))
-#fin_pre_land.to_netcdf(f'{cfg.path_in}/{wrun_pre}/hist2d_IFD_resample_time_2013-2020_land_mean_{reg}.nc')
-#fin_fut_land.to_netcdf(f'{cfg.path_in}/{wrun_fut}/hist2d_IFD_resample_time_2013-2020_land_mean_{reg}.nc')
+if reg=='':
 
-#Domain
-#fin_pre_land = fin_pre.where(lm_is==1).sum(dim=('x','y'))
-#fin_pre_land.to_netcdf(f'{cfg.path_in}/{wrun_pre}/hist2d_IFD_spell_hist2d_2013-2020_land_sum.nc')
-fin_fut_land = fin_fut.where(lm_is==1).sum(dim=('x','y'))
-fin_fut_land.to_netcdf(f'{cfg.path_in}/{wrun_fut}/hist2d_IFD_spell_hist2d_2013-2020_land_sum.nc')
-#fin_fut_land.to_netcdf(f'{cfg.path_in}/{wrun_fut}/hist2d_IFD_resample_time_2013-2020_land_sum.nc')
+    fin_pre_land = fin_pre.where(lm_is==1).sum(dim=('x','y'))
+    fin_fut_land = fin_fut.where(lm_is==1).sum(dim=('x','y'))
+    fin_pre_land.to_netcdf(f'{cfg.path_in}/{wrun_pre}/hist2d_resample_time_IFD_2013-2020_land_sum.nc')
+    fin_fut_land.to_netcdf(f'{cfg.path_in}/{wrun_fut}/hist2d_resample_time_IFD_2013-2020_land_sum.nc')
+
+
+else:
+    fin_pre_land = fin_pre.where(lm_is==1).sel(y=slice(cfg.reg_coords[reg][0],cfg.reg_coords[reg][2]),x=slice(cfg.reg_coords[reg][1],cfg.reg_coords[reg][3])).sum(dim=('x','y'))
+    fin_fut_land = fin_fut.where(lm_is==1).sel(y=slice(cfg.reg_coords[reg][0],cfg.reg_coords[reg][2]),x=slice(cfg.reg_coords[reg][1],cfg.reg_coords[reg][3])).sum(dim=('x','y'))
+    fin_pre_land.to_netcdf(f'{cfg.path_in}/{wrun_pre}/hist2d_resample_time_IFD_2013-2020_land_sum_{reg}.nc')
+    fin_fut_land.to_netcdf(f'{cfg.path_in}/{wrun_fut}/hist2d_resample_time_IFD_2013-2020_land_sum_{reg}.nc')
