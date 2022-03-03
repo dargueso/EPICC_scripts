ed1004dfb76777e8a0dc353b7f60dd0db6a3607b
Daniel Argüeso <d.argueso@unsw.edu.au>
2022-03-03 12:34:07 +0100

    Update epicc_config.py

--- epicc_config.py
+++ epicc_config.py
@@ -20,14 +20,15 @@ smonth = 8
 emonth = 12
 
 qtiles = [0.1,0.2,0.25,0.4,0.5,0.6, 0.7,0.8,0.9,0.95,0.98,0.99,0.999,0.9999,1.]
-wet_value = 0.1 #mm
+wet_value = 1 #mm
 wrf_runs = ['EPICC_2km_ERA5_HVC_GWD']#,'EPICC_2km_ERA5_CMIP6anom_HVC_GWD']
 
 
 reg_coords = {'BAL':[38.6,0.9,40.3,4.7],
               'LEV':[36.4,-3.30,40.20,1.0],
               'CAT':[40.3,0,43,3.5],
-              'SAR':[38.7,7.74,41.35,10.3]}
+              'SAR':[38.7,7.74,41.35,10.3],
+              'WME':[36,-5,46,15]}
 
 crosssect_coords = {'BAL':[38.8,0.95,40.2,4.4],
                     'CAT':None}
