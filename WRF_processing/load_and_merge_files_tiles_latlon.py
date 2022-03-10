98c4ae11cd1eba9da791bd8241efd8e5df3de1b7
Daniel Argüeso <d.argueso@unsw.edu.au>
2022-03-10 11:44:36 +0100

    Adapted to new file naming and methods to calculate IFD hist2d

--- WRF_processing/load_and_merge_files_tiles_latlon.py
+++ WRF_processing/load_and_merge_files_tiles_latlon.py
@@ -38,45 +38,24 @@ tile_size = 50
 def main():
 
     """  Split files into tiles """
-#RECURSIVE
-# for fr in ['10MIN','01H','DAY']:
-#     for seas in ['DJF','MAM','JJA','SON']:
-#         filespath = f'{cfg.path_in}/{wrun}/ptiles_tiles_50/UIB_{fr}_RAIN_2013-2020'
-#         filessuffix = f'_qtiles_all'
-#         filesin = sorted(glob(f'{cfg.path_in}/{wrun}/{cfg.patt_in}_10MIN_RAIN_20??-??.nc'))
-#         files_ref = xr.open_dataset(filesin[0])
-#         nlats = files_ref.sizes['y']
-#         nlons = files_ref.sizes['x']
-#
-#         latlongrid = []
-#         for nnlat in range(nlats//tile_size+1):
-#           print(nnlat)
-#           latlongrid.append([f'{filespath}_{nnlat:03d}y-{nnlon:03d}x{filessuffix}.nc' for nnlon in range(nlons//tile_size+1)])
-#         fin_all = xr.open_mfdataset(latlongrid,combine='nested',concat_dim=["y", "x"]).load()
-#         print(f'Ej: {filespath}_000y-000x{filessuffix}.nc')
-#         fout = latlongrid[0][0].replace("_000y-000x",f'')
-#         print(f'Output file: {fout}')
-#         fin_all.to_netcdf(fout)
 
-#SINGLE FILE TYPES
-#filespath = f'{cfg.path_in}/{wrun}/hist2d_IFD_tiles_50/hist2d_IFD_spell_hist2d_2013-2020'
-filespath = f'{cfg.path_in}/{wrun}/hist2d_resample_time_IFD_2013-2020'
+    filespath = f'{cfg.path_in}/{wrun}/hist2d_spell_IFD_2013-2020'
 
-filessuffix = f''
-filesin = sorted(glob(f'{cfg.path_in}/{wrun}/{cfg.patt_in}_10MIN_RAIN_20??-??.nc'))
-files_ref = xr.open_dataset(filesin[0])
-nlats = files_ref.sizes['y']
-nlons = files_ref.sizes['x']
+    filessuffix = f'_1.0mm'
+    filesin = sorted(glob(f'{cfg.path_in}/{wrun}/{cfg.patt_in}_10MIN_RAIN_20??-??.nc'))
+    files_ref = xr.open_dataset(filesin[0])
+    nlats = files_ref.sizes['y']
+    nlons = files_ref.sizes['x']
 
-latlongrid = []
-for nnlat in range(nlats//tile_size+1):
-  print(nnlat)
-  latlongrid.append([f'{filespath}_{nnlat:03d}y-{nnlon:03d}x{filessuffix}.nc' for nnlon in range(nlons//tile_size+1)])
-fin_all = xr.open_mfdataset(latlongrid,combine='nested',concat_dim=["y", "x"]).load()
-print(f'Ej: {filespath}_000y-000x{filessuffix}.nc')
-fout = latlongrid[0][0].replace("_000y-000x",f'')
-print(f'Output file: {fout}')
-fin_all.to_netcdf(fout)
+    latlongrid = []
+    for nnlat in range(nlats//tile_size+1):
+      print(nnlat)
+      latlongrid.append([f'{filespath}_{nnlat:03d}y-{nnlon:03d}x{filessuffix}.nc' for nnlon in range(nlons//tile_size+1)])
+    fin_all = xr.open_mfdataset(latlongrid,combine='nested',concat_dim=["y", "x"]).load()
+    print(f'Ej: {filespath}_000y-000x{filessuffix}.nc')
+    fout = latlongrid[0][0].replace("_000y-000x",f'')
+    print(f'Output file: {fout}')
+    fin_all.to_netcdf(fout)
 
 ###############################################################################
 ##### __main__  scope
