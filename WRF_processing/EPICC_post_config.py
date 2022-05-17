675c4f6876abfc84188da065b1707799c8d34542
Daniel Argueso <d.argueso@uib.es>
2022-05-17 15:31:35 +0200

    Minor changes

--- WRF_processing/EPICC_post_config.py
+++ WRF_processing/EPICC_post_config.py
@@ -8,19 +8,19 @@ path_geo = '/home/dargueso/share/geo_em_files/EPICC/'
 file_geo= 'geo_em.d01.EPICC_2km_ERA5_HVC_GWD.nc'
 file_ref = 'wrfout_d01_2020-08-01_00:00:00'
 institution = 'UIB'
-wruns = ['EPICC_2km_ERA5_HVC_GWD']#,'EPICC_2km_ERA5_CMIP6anom_HVC_GWD']
+wruns = ['EPICC_2km_ERA5_CMIP6anom_HVC_GWD']#,'EPICC_2km_ERA5_CMIP6anom_HVC_GWD']
 
 
 patt = 'wrf3hrly'
 dom  = 'd01'
 
-syear = 2018
+syear = 2013
 eyear = 2020
 smonth =1
 emonth =12
 acc_dt = 10
 
-variables = ['PVO']
+variables = ['CAPE2D']
 
 #### Requested output variables (DO NOT CHANGE THIS LINE) ####
 
