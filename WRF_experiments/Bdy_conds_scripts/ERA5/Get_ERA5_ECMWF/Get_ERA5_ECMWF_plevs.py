aeb2ac131e8e837fa14aef806a462adde2b7ac76
Daniel Argüeso <d.argueso@uib.es>
2025-05-12 15:25:56 +0200

    Different pathways for creating ERA5 - deprecated

--- WRF_experiments/Bdy_conds_scripts/ERA5/Get_ERA5_ECMWF/Get_ERA5_ECMWF_plevs.py
+++ WRF_experiments/Bdy_conds_scripts/ERA5/Get_ERA5_ECMWF/Get_ERA5_ECMWF_plevs.py
@@ -15,14 +15,14 @@ def retrieve_era5():
     You can use the variable 'target' to organise the requested data in files as you wish.
     In the example below the data are organised in files per day. (eg "era5_daily_20151001.grb")
     """
-    yearStart = 2022
-    yearEnd = 2022
-    monthStart =1 
+    yearStart = 2015
+    yearEnd = 2016
+    monthStart = 7
     monthEnd = 12
 
     y = yearStart
     m = monthStart
-    d = 1 
+    d = 1
 
     while y < yearEnd or (y == yearEnd and m <= monthEnd):
 
@@ -97,7 +97,7 @@ def era5_request(year, month, day, target):
             "year": "%s" % (year),
             "month": "%02d" % (month),
             "day": "%02d" % (day),
-            "time": ["00:00", "03:00", "06:00","09:00", "12:00","15:00", "18:00","21:00"],
+            "time": ["00:00", "06:00", "12:00", "18:00"],
             "grid": [0.3, 0.3],
         },
         target,
--- WRF_experiments/Bdy_conds_scripts/ERA5/Get_ERA5_ECMWF/Get_ERA5_ECMWF_sfc.py
+++ WRF_experiments/Bdy_conds_scripts/ERA5/Get_ERA5_ECMWF/Get_ERA5_ECMWF_sfc.py
@@ -15,14 +15,14 @@ def retrieve_era5():
     You can use the variable 'target' to organise the requested data in files as you wish.
     In the example below the data are organised in files per day. (eg "era5_daily_20151001.grb")
     """
-    yearStart = 2010
-    yearEnd = 2011
-    monthStart =12 
-    monthEnd = 1
+    yearStart = 2015
+    yearEnd = 2015
+    monthStart =8 
+    monthEnd = 12
 
     y = yearStart
     m = monthStart
-    d = 22
+    d = 1
 
     while y < yearEnd or (y == yearEnd and m <= monthEnd):
 
@@ -72,7 +72,7 @@ def era5_request(year, month, day, target):
             "year": "%s" % (year),
             "month": "%02d" % (month),
             "day": "%02d" % (day),
-            "time": ["00:00", "03:00", "06:00","09:00", "12:00","15:00", "18:00","21:00"], 
+            "time": ["00:00", "06:00", "12:00", "18:00"],
             "grid": [0.3, 0.3],
         },
         target,
