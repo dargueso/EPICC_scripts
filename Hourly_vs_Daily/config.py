839337e1d8b53415d11fd6446abb3d7a1e7a9161
Daniel Argüeso <d.argueso@uib.es>
2025-07-22 13:41:02 +0200

    Update config.py
    Changed buffer and added tile size

--- Hourly_vs_Daily/config.py
+++ Hourly_vs_Daily/config.py
@@ -2,11 +2,13 @@ import xarray as xr
 import numpy as np
 
 path_in = '/home/dargueso/postprocessed/EPICC/'
+path_out = '/home/dargueso/postprocessed/EPICC/'
 WET_VALUE_H = 0.1  # mm
 WET_VALUE_D = 1.0 # mm
 drain_bins = np.concatenate((np.arange(1, 10, 1), np.arange(10, 105, 5)))
 hrain_bins = np.concatenate((np.arange(0, 10, 1), np.arange(10, 105, 5)))
-buffer = 20
+buffer = 25
+tile_size = 50
 n_samples = 100
 
 # Define quantiles for the analysis
