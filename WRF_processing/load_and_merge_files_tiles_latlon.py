ba667e17ab977e45513de63bb0a41edda635ef2f
Daniel Argüeso <d.argueso@uib.es>
2025-07-31 13:36:16 +0200

    Update load_and_merge_files_tiles_latlon.py
    Added flexibility, now multiple file patterns at once

--- WRF_processing/load_and_merge_files_tiles_latlon.py
+++ WRF_processing/load_and_merge_files_tiles_latlon.py
@@ -23,8 +23,8 @@ import numpy as np
 import epicc_config as cfg
 from glob import glob
 
-# wrun = cfg.wrf_runs[0]
-wrun = 'EPICC_2km_ERA5_CMIP6anom_HVC_GWD'
+wrun = cfg.wrf_runs[0]
+#wrun = 'EPICC_2km_ERA5_CMIP6anom'
 tile_size = 50
 
 # #filespath = f'{cfg.path_in}/{wrun}/{cfg.patt_in}_{freq}_RAIN_2013-2020'
@@ -38,24 +38,32 @@ tile_size = 50
 def main():
 
     """  Split files into tiles """
+    filespatterns = [#f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_NDI',
+                     #f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_top100_NDI',
+                     f'{cfg.path_in}/{wrun}/probability_hourly',
+                    #  f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_NDI_DJF',
+                    #  f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_NDI_MAM',
+                    #  f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_NDI_JJA',
+                    #  f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_NDI_SON',
+                     ]
+    for filespath in filespatterns:
+      #filespath = f'{cfg.path_in}/{wrun}/split_files_tiles_50/Hourly_decomposition_NDI_SON'
 
-    filespath = f'{cfg.path_in}/{wrun}/hist2d_spell_sumevents_IFD_2013-2020'
+      filessuffix = f'_025buffer'
+      filesin = sorted(glob(f'{cfg.path_in}/{wrun}/{cfg.patt_in}_01H_RAIN_20??-??.nc'))
+      files_ref = xr.open_dataset(filesin[0])
+      nlats = files_ref.sizes['y']
+      nlons = files_ref.sizes['x']
 
-    filessuffix = f''
-    filesin = sorted(glob(f'{cfg.path_in}/{wrun}/{cfg.patt_in}_10MIN_RAIN_20??-??.nc'))
-    files_ref = xr.open_dataset(filesin[0])
-    nlats = files_ref.sizes['y']
-    nlons = files_ref.sizes['x']
-
-    latlongrid = []
-    for nnlat in range(nlats//tile_size+1):
-      print(nnlat)
-      latlongrid.append([f'{filespath}_{nnlat:03d}y-{nnlon:03d}x{filessuffix}.nc' for nnlon in range(nlons//tile_size+1)])
-    fin_all = xr.open_mfdataset(latlongrid,combine='nested',concat_dim=["y", "x"]).load()
-    print(f'Ej: {filespath}_000y-000x{filessuffix}.nc')
-    fout = latlongrid[0][0].replace("_000y-000x",f'')
-    print(f'Output file: {fout}')
-    fin_all.to_netcdf(fout)
+      latlongrid = []
+      for nnlat in range(nlats//tile_size+1):
+        print(nnlat)
+        latlongrid.append([f'{filespath}_{nnlat:03d}y-{nnlon:03d}x{filessuffix}.nc' for nnlon in range(nlons//tile_size+1)])
+      fin_all = xr.open_mfdataset(latlongrid,combine='nested',concat_dim=["y", "x"]).load()
+      print(f'Ej: {filespath}_000y-000x{filessuffix}.nc')
+      fout = latlongrid[0][0].replace("_000y-000x",f'')
+      print(f'Output file: {fout}')
+      fin_all.to_netcdf(fout)
 
 ###############################################################################
 ##### __main__  scope
