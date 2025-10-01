Modified hourly bins and fix calculation of hist

Hourly_vs_Daily/create_hourly_daily_probabilities_tiles.py
@@ -19,7 +19,7 @@ import multiprocessing
 # Configuration
 PATH_IN = '/home/dargueso/postprocessed/EPICC/'
 PATH_OUT = '/home/dargueso/postprocessed/EPICC/'
-WRUN = "EPICC_2km_ERA5"
+WRUN = "EPICC_2km_ERA5_CMIP6anom"
 
 # Parallel processing configuration
 N_JOBS = 10  # Number of tiles to process simultaneously (adjust based on your system)
@@ -92,7 +92,7 @@ PATH_OUT = '/home/dargueso/postprocessed/EPICC/'
 WET_VALUE_H = 0.1
 WET_VALUE_D = 1.0
 DRAIN_BINS = np.arange(0, 105, 5)
-HRAIN_BINS = np.arange(0, 105, 5)
+HRAIN_BINS = np.arange(0, 101, 1)
 
 try:
     from numba import jit
@@ -118,8 +118,8 @@ try:
         
         total = np.sum(hist)
         if total > 0:
-            bin_widths = bins[1:] - bins[:-1]
-            hist = hist / total * bin_widths
+            #bin_widths = bins[1:] - bins[:-1]
+            hist = hist / total 
         
         return hist
 
@@ -138,8 +138,8 @@ def compute_histogram_numpy(values, bins):
     
     total = hist.sum()
     if total > 0:
-        bin_widths = np.diff(bins)
-        hist = hist.astype(float) / total * bin_widths
+        #bin_widths = np.diff(bins)
+        hist = hist.astype(float) / total
     
     return hist
 
@@ -237,7 +237,7 @@ def calculate_wet_hour_intensity_distribution_optimized(ds_h_wet_days, ds_d, wet
 
     return hourly_distribution_bin, wet_hours_distribution_bin, samples_per_bin
 
-wrun = "EPICC_2km_ERA5"
+wrun = "EPICC_2km_ERA5_CMIP6anom"
 TILE_ID = "REPLACE_TILE_ID"
 file_pattern = f'{PATH_IN}/{wrun}/split_files_tiles_50/UIB_01H_RAIN_20??-??_{TILE_ID}.nc'
 output_file = f'{PATH_OUT}/{wrun}/rainfall_probability_optimized_conditional_{TILE_ID}.nc'